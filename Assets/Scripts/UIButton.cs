8453a971ce5d6ea2d069fdcdae237c5e155bec4f
LeHoevoAaron <aaronshenglu@gmail.com>
2018-05-24 01:52:02 -0400

    added a gamemanager static script and starting to redo menu screen

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButton : MonoBehaviour
+{
+	public int ButtonIndex;
+	private Button _butt;
+
+	void Start()
+	{
+		//UnbreakableManager.Feesh
+	}
+
+	// Update is called once per frame
+	public void ChooseAFish()
+	{
+		if (UnbreakableManager.OrangeFeesh == UnbreakableManager.OrangeFishChosen.Undecided)
+		{
+			if (ButtonIndex == 1)
+			{
+				UnbreakableManager.OrangeFeesh = UnbreakableManager.OrangeFishChosen.OrangeBrideChosen;
+			}
+
+			if (ButtonIndex == 2)
+			{
+				UnbreakableManager.OrangeFeesh = UnbreakableManager.OrangeFishChosen.OrangeGroomChosen;
+			}
+		}
+
+		if (UnbreakableManager.GreenFeesh == UnbreakableManager.GreenFishChosen.Undecided)
+		{
+			if (ButtonIndex == 3)
+			{
+				UnbreakableManager.GreenFeesh = UnbreakableManager.GreenFishChosen.GreenBrideChosen;
+			}
+
+			if (ButtonIndex == 4)
+			{
+				UnbreakableManager.GreenFeesh = UnbreakableManager.GreenFishChosen.GreenGroomChosen;
+			}
+		}	
+	}
+}
---
+++ Assets/Scripts/UnbreakableManager.cs
@@ -0,0 +1,39 @@
+﻿ using System;
+ using UnityEngine;
+using System.Collections;
+
+public class UnbreakableManager : MonoBehaviour
+{
+ private static UnbreakableManager _manager;
+
+ public enum OrangeFishChosen
+ {
+  Undecided,
+  OrangeBrideChosen,
+  OrangeGroomChosen,
+ };
+
+ public enum GreenFishChosen
+ {
+  Undecided,
+  GreenBrideChosen,
+  GreenGroomChosen
+ };
+
+ public static OrangeFishChosen OrangeFeesh;
+ public static GreenFishChosen GreenFeesh;
+
+ void Awake()
+ {
+  if (_manager == null)
+  {
+   DontDestroyOnLoad(this);
+  }
+  else
+  {
+   Destroy(gameObject);
+  }
+ }
+}
+  
+    
