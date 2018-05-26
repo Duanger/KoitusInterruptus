94253bcf1b14db9e014170f04af94880b109d8a1
LeHoevoAaron <aaronshenglu@gmail.com>
2018-05-26 15:34:51 -0400

    generalized UI code for all possible up down left right inputs, now need to make monobehaviours for each submenu

--- Assets/Scripts/MenuUiInputHandler.cs
+++ Assets/Scripts/MenuUiInputHandler.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using SinputSystems;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 using UnityScript.Macros;
 
@@ -10,87 +12,144 @@ public class MenuUiInputHandler : MonoBehaviour
 {
 
 	public int CurrentMenuButtonIndex;
+	[SerializeField] private Color _highlightedColor;
 	[SerializeField] private EventSystem _uiEventSystem;
 	[SerializeField] private Image[] _mainButts = new Image[3];
 	private bool _triggered;
 	private float _axes;
+	[SerializeField] private String[] _axeStrings = new string[4];
+	[SerializeField] private Vector3 _smallSize, _highlightedSize;
 	private InputDeviceSlot AnySticks = InputDeviceSlot.any;
-	void Start ()
-	{
-		Mathf.Clamp(CurrentMenuButtonIndex, 0, 2);
-	}
+	private InputDeviceSlot _player1 = InputDeviceSlot.gamepad1;
+	private InputDeviceSlot _player2 = InputDeviceSlot.gamepad2;
 
+	public void InputPlayerHandler(bool anyPlayer,int switchy, int switchy2, int numOfAxes,Image[] buttonImage)
+	{
+		if (switchy < 0)
+		{
+			switchy = buttonImage.Length;
+		}
+
+		if (switchy > buttonImage.Length)
+		{
+			switchy = 0;
+		}
+		if (anyPlayer)
+		{
+			if (numOfAxes == 2)
+			{
+				if (Sinput.GetButtonDownRepeating(_axeStrings[0], AnySticks))
+				{
+					switchy--;
+				}
+
+				if (Sinput.GetButtonDownRepeating(_axeStrings[1], AnySticks))
+				{
+					switchy++;
+				}
+				EventSystem.current.SetSelectedGameObject(buttonImage[switchy].gameObject);
+			}
+
+			if (numOfAxes == 4)
+			{
+				if (Sinput.GetButtonDownRepeating(_axeStrings[0], AnySticks))
+				{
+					switchy--;
+				}
+
+				if (Sinput.GetButtonDownRepeating(_axeStrings[1], AnySticks))
+				{
+					switchy++;
+				}
+				if (Sinput.GetButtonDownRepeating(_axeStrings[2], AnySticks))
+				{
+					switchy2--;
+				}
+
+				if (Sinput.GetButtonDownRepeating(_axeStrings[3], AnySticks))
+				{
+					switchy2++;
+				}
+				
+			}
+			
+			foreach (var b in buttonImage)
+			{
+				if (switchy == System.Array.IndexOf(buttonImage, b) && Sinput.GetButtonDown("Submit", AnySticks))
+				{
+					ExecuteEvents.Execute(b.gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.submitHandler);
+				}
+			}
+		}
+		else
+		{
+			if (numOfAxes == 2)
+			{
+				if (Sinput.GetButtonDownRepeating(_axeStrings[2], _player1))
+				{
+					switchy--;
+				}
+
+				if (Sinput.GetButtonDownRepeating(_axeStrings[2], _player2))
+				{
+					switchy2--;
+				}
+
+				if (Sinput.GetButtonDownRepeating(_axeStrings[3], AnySticks))
+				{
+					switchy2--;
+				}
+				if(Sinput.GetButtonDownRepeating(_axeStrings[3],_player2))
+				{
+					switchy2++;
+				}
+				foreach (var b in buttonImage)
+				{
+					/*if (switchy == System.Array.IndexOf(buttonImage, b) && Sinput.GetButtonDown("Submit", _player1))
+					{
+						ExecuteEvents.Execute(b.gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.submitHandler);
+					}
+					if (switchy == System.Array.IndexOf(buttonImage, b) && Sinput.GetButtonDown("Submit", _player2)
+					{
+						ExecuteEvents.Execute(b.gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.submitHandler);
+					}*/
+				}
+			}
+		}
+
+		
+	}
 	void Update ()
 	{
-		
-		//Debug.Log(Sinput.GetAxis("Horizontal", _player1));
-		_axes = Sinput.GetAxis("Horizontal", AnySticks);
-	
-		if (CurrentMenuButtonIndex < 0)
-		{
-			CurrentMenuButtonIndex = 2;
-		}
-
-		if (CurrentMenuButtonIndex > 2)
-		{
-			
-			CurrentMenuButtonIndex = 0;
-		}
-		if(Sinput.GetButtonDownRepeating("Left", AnySticks))
-		{
-			CurrentMenuButtonIndex--;
-		}
-		if (Sinput.GetButtonDownRepeating("Right", AnySticks))
-		{
-			CurrentMenuButtonIndex++;
-		}
-		
-		/*if (_axes == 0 && _triggered)
-		{
-			_triggered = false;
-		}
-		if (_axes > 0 && !_triggered)
-		{
-			_triggered = true;
-			CurrentMenuButtonIndex++;
-		}
-		
-
-		if (_axes < 0 && !_triggered)
-		{
-			_triggered = true;
-			CurrentMenuButtonIndex--;
-		}*/
-        //var pointer = new PointerEventData(EventSystem.current);
-		/*if (Sinput.GetButton("Submit", AnySticks) && CurrentMenuButtonIndex)
-		{
-			ExecuteEvents.Execute(_start.gameObject, pointer, ExecuteEvents.submitHandler);
-		}*/
-
 			switch (CurrentMenuButtonIndex)
 			{
 				case 0:
 					_uiEventSystem.SetSelectedGameObject(_mainButts[0].gameObject);
-					Debug.Log(_mainButts[0].ToString() + "is pressed");
-					_mainButts[0].rectTransform.localScale = new Vector3(1.7f, 1.7f, 1.7f);
-					_mainButts[1].rectTransform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-					_mainButts[2].rectTransform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+					_mainButts[0].rectTransform.localScale = _highlightedSize;
+					_mainButts[1].rectTransform.localScale = _smallSize;
+					_mainButts[2].rectTransform.localScale = _smallSize;
+					_mainButts[0].color = _highlightedColor;
+					_mainButts[1].color = Color.white;
+					_mainButts[2].color = Color.white;
 					break;
 				case 1:
 					_uiEventSystem.SetSelectedGameObject(_mainButts[1].gameObject);
-					Debug.Log(_mainButts[1].ToString() + "is pressed");
-					_mainButts[1].rectTransform.localScale = new Vector3(1.7f, 1.7f, 1.7f);
-					_mainButts[0].rectTransform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-					_mainButts[2].rectTransform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+					_mainButts[1].rectTransform.localScale = _highlightedSize;
+					_mainButts[0].rectTransform.localScale = _smallSize;
+					_mainButts[2].rectTransform.localScale = _smallSize;
+					_mainButts[1].color = _highlightedColor;
+					_mainButts[0].color = Color.white;
+					_mainButts[2].color = Color.white;
 					break;
 				case 2:
 					_uiEventSystem.SetSelectedGameObject(_mainButts[2].gameObject);
-					Debug.Log(_mainButts[2].ToString() + "is pressed");
-					_mainButts[2].rectTransform.localScale = new Vector3(1.7f, 1.7f, 1.7f);
-					_mainButts[0].rectTransform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-					_mainButts[1].rectTransform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+					_mainButts[2].rectTransform.localScale = _highlightedSize;
+					_mainButts[0].rectTransform.localScale = _smallSize;
+					_mainButts[1].rectTransform.localScale = _smallSize;
+					_mainButts[2].color = _highlightedColor;
+					_mainButts[0].color = Color.white;
+					_mainButts[1].color = Color.white;
 					break;
-			}
-		
+			}		
 	}
 }
---
+++ Assets/Scripts/StartMenuUiInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SinputSystems;
+using UnityEngine;
+
+public class StartMenuUiInput : MonoBehaviour
+{
+
+	private MenuUiInputHandler _menuHandler;
+	private InputDeviceSlot _playerOneSlot = InputDeviceSlot.gamepad1;
+	private InputDeviceSlot _playerTwoSlot = InputDeviceSlot.gamepad2;
+	void Start ()
+	{
+		_menuHandler = GetComponent<MenuUiInputHandler>();
+		_menuHandler.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
