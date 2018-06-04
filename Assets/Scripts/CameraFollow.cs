e01fea13309fe9a8d8b219e1bf623c763172930f
LeHoevoAaron <aaronshenglu@gmail.com>
2018-06-03 23:18:38 -0400

    did some things

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,21 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraFollow : MonoBehaviour
 {
-
+	private bool _doOnceInUpdate;
+	[SerializeField] private bool _isBag;
+	private float _currentLerpTime;
+	private float _bagCurrentTime;
+	[SerializeField] private float _timeToLerp;
 	[SerializeField] private GameObject _fish1,_fish2;
+	[SerializeField] private GameObject _cvas;
+	[SerializeField] private Vector3 _targetVector;
+	
+	public Image Bagginton;   
+    public Vector3 _bagTargetVector;
+
+
+	public bool Started;
 
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 targetPos = new Vector3((_fish1.transform.position.x + _fish2.transform.position.x)/2
-			,(_fish1.transform.position.y + _fish2.transform.position.y)/2,-10);
-		transform.position = Vector3.Lerp(transform.position, targetPos, 0.6f);
+		if (_isBag)
+		{
+			if (Started)
+			{
+				_currentLerpTime += Time.deltaTime;  
+				if (_currentLerpTime >= _timeToLerp)   
+				{                                      
+					_currentLerpTime = _timeToLerp;    
+				}
 
+				float fractal = _currentLerpTime / _timeToLerp;
+				transform.position = Vector3.Lerp(transform.position,_targetVector,fractal);
+				if (transform.position == _targetVector)
+				{
+					_bagCurrentTime += Time.deltaTime;
+					if (_bagCurrentTime >= _timeToLerp*5f)
+					{
+						_bagCurrentTime = _timeToLerp * 5f;
+					}
+
+					float myGoat = _bagCurrentTime / (_timeToLerp * 5f);
+					Bagginton.rectTransform.anchoredPosition = Vector3.Lerp(Bagginton.rectTransform.anchoredPosition, _bagTargetVector, myGoat);
+					if (Bagginton.rectTransform.anchoredPosition.y < -442)
+					{
+	                  _cvas.SetActive(true);
+					}
+				}
+			}
+		}
+		
+		if (!_isBag)
+		{
+			Vector3 targetPos = new Vector3((_fish1.transform.position.x + _fish2.transform.position.x)/2
+				,(_fish1.transform.position.y + _fish2.transform.position.y)/2,-10);
+			transform.position = Vector3.Lerp(transform.position, targetPos, 0.6f);
+		}
 	}
 }
--- Assets/Scripts/UIButton.cs
+++ Assets/Scripts/UIButton.cs
@@ -1,31 +1,36 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEditor.Experimental.UIElements.GraphView;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class UIButton : MonoBehaviour
 {
 	public int ButtonIndex;
 	[SerializeField]private Animator _anim;
+	private Animator _imAnim;
+	private CameraFollow _cm;
 	[SerializeField] private bool _isFishButton;
 	private GameObject _anyEventSystem, _eventSystem1, _eventSystem2;
 	private bool _testBool;
-	private GameObject _herChoice;
+	private bool _orangeBool, _greenBool;
+	[SerializeField]private GameObject _herChoice;
 	[SerializeField] private bool _testTwoControllers;
+	private float _currentTimeOrange;
+	private float _currentTimeGreen;
+	[SerializeField] private float _oLength, _gLength;
+	private Image _im;
+	private Image _iman;
 	private String _fishy;
 
 	private void Start()
 	{
-		
-		
+		_cm = Camera.main.gameObject.GetComponent<CameraFollow>();
 		if (_isFishButton)
 		{
-			_anim.enabled = false;
+			_im = GetComponent<Image>();
+			_iman = GetComponentInChildren<Image>();
+			_imAnim = GetComponent<Animator>();
 		}
 		if (gameObject.name == "Start")
 		{
@@ -61,25 +66,24 @@ public class UIButton : MonoBehaviour
 
 	public void PressedStart()
 	{
+		_cm.Started = true;
 		_anyEventSystem.SetActive(false);
 		_eventSystem1.SetActive(true);
 		_eventSystem2.SetActive(true);
-		_herChoice.SetActive(true);
 		transform.parent.gameObject.SetActive(false);
 		//UnbreakableManager.Feesh
 	}
 
 	public void Selectedd()
 	{
-		_anim.enabled = true;
+		_anim.SetBool("highlighted",true);
 		//_testBool = true;
 
 	}
 
 	public void Deselected()
 	{
-		
-		_anim.enabled = false;
+		_anim.SetBool("highlighted",false);
 		/*if (_isFishButton)
 		{
 			_anim.enabled = false;
@@ -92,7 +96,52 @@ public class UIButton : MonoBehaviour
 		{
 			SceneManager.LoadScene(1);
 		}
+		if (ButtonIndex == 1 && _im.canvasRenderer.GetAlpha() == 0|| ButtonIndex == 2 && _im.canvasRenderer.GetAlpha() == 0)
+		{
+			_orangeBool = true;
+		}
+		if (ButtonIndex == 3 && _im.canvasRenderer.GetAlpha() == 0|| ButtonIndex == 4 && _im.canvasRenderer.GetAlpha() == 0)
+		{
+			_greenBool = true;
+		}
 
+		if (_orangeBool && ButtonIndex > 0)
+		{
+			Vector2 targetPosition = new Vector2(363, 126);
+			Vector2 targetSize = new Vector2(1.4f,1.627907f);
+			_currentTimeOrange += Time.deltaTime;
+			if (_currentTimeOrange >= _oLength)
+			{
+				_currentTimeOrange = _oLength;
+			}
+
+			float p = _currentTimeOrange / _oLength;
+			_iman.rectTransform.localScale= Vector2.Lerp(_iman.rectTransform.localScale, targetSize, p);
+			_im.rectTransform.anchoredPosition = Vector2.Lerp(_im.rectTransform.anchoredPosition,targetPosition,p);
+			if (_im.rectTransform.anchoredPosition == targetPosition)
+			{
+				GetComponent<Button>().interactable = false;
+			}
+		}
+		if (_greenBool && ButtonIndex > 0)
+		{
+			Vector2 targetPosition = new Vector2(363, 126);
+			Vector2 targetSize = new Vector2(1.4f,1.627907f);
+			_currentTimeGreen += Time.deltaTime;
+			if (_currentTimeOrange >= _gLength)
+			{
+				_currentTimeGreen= _gLength;
+			}
+
+			float d = _currentTimeGreen / _gLength;
+			_iman.rectTransform.localScale = Vector2.Lerp(_iman.rectTransform.localScale, targetSize, d);
+			_im.rectTransform.anchoredPosition = Vector2.Lerp(_im.rectTransform.anchoredPosition,targetPosition,d);
+			
+			if (_im.rectTransform.anchoredPosition == targetPosition)
+			{
+				GetComponent<Button>().interactable = false;
+			}
+		}
 		
 	}
 
@@ -103,14 +152,18 @@ public class UIButton : MonoBehaviour
 		{
 			if (ButtonIndex == 1)
 			{
-				
-				
 				UnbreakableManager.OrangeFeesh = UnbreakableManager.OrangeFishChosen.OrangeBrideChosen;
+				_anim.enabled = false;
+				_imAnim.enabled = false;
+				_im.CrossFadeAlpha(0f,0.7f,false);
 			}
 
 			if (ButtonIndex == 2)
 			{
 				UnbreakableManager.OrangeFeesh = UnbreakableManager.OrangeFishChosen.OrangeGroomChosen;
+				_anim.enabled = false;
+				_imAnim.enabled = false;
+				_im.CrossFadeAlpha(0f,0.7f,false);
 			}
 		}
 
@@ -119,11 +172,17 @@ public class UIButton : MonoBehaviour
 			if (ButtonIndex == 3)
 			{
 				UnbreakableManager.GreenFeesh = UnbreakableManager.GreenFishChosen.GreenBrideChosen;
+				_anim.enabled = false;
+				_imAnim.enabled = false;
+				_im.CrossFadeAlpha(0f,0.7f,false);	
 			}
 
 			if (ButtonIndex == 4)
 			{
 				UnbreakableManager.GreenFeesh = UnbreakableManager.GreenFishChosen.GreenGroomChosen;
+				_anim.enabled = false;
+				_imAnim.enabled = false;
+				_im.CrossFadeAlpha(0f,0.7f,false);
 			}
 		}	
 	}
